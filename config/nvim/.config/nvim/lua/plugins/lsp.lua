3725b44ce1e58ae87dbe9cc87b2ac25db7e422b3
Simi Falaye <simifalaye1@gmail.com>
2023-05-01 22:08:17 -0600

    neovim lsp diagnostics

--- config/nvim/.config/nvim/lua/plugins/lsp.lua
+++ config/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -64,6 +64,43 @@ local autocmds = function(client, _)
   end
 end
 
+local diagnostics = function()
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
+
+  -- Define signs
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(
+    sign.name,
+    { texthl = sign.name, text = sign.text, numhl = "" }
+    )
+  end
+  -- Configure diagnostics
+  vim.diagnostic.config({
+    virtual_text = true,
+    signs = { active = signs },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  })
+  vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/signatureHelp"] =
+  vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -84,6 +121,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local null_ls = require("null-ls")
+      diagnostics()
 
       -- Default server options
       local default_opts = {
