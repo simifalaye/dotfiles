d59a91ce0894f76a5f797615f350ac217cf9d543
Simi Falaye <simifalaye1@gmail.com>
2024-04-19 12:48:19 -0600

    Github cli, dash and octo

---
+++ roles/neovim/dots/.config/nvim/lua/plugins/img_clip.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image" },
+    },
+    opts = {},
+  },
+}
