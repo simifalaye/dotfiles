acca1f0f078b69b5a612ffb5bdfce172e49db02a
Simi Falaye <simi.falaye@sophos.com>
2024-11-10 16:53:54 -0600

    latest nvim changes

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -9,7 +9,6 @@
 in {
   home.packages = with pkgs; [
     luajit
-    luajitPackages.luarocks
     unstable.neovim
   ];
 
--- home-manager/neovim/dots/.config/nvim/lua/plugins/blink-cmp.lua
+++ home-manager/neovim/dots/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,9 +1,33 @@
-require("blink-cmp").setup({
-  windows = {
-    documentation = {
-      auto_show = true,
-      auto_show_delay_ms = 500,
-      update_delay_ms = 50,
+local lz = require("utils.lazy").new("blink-cmp", function()
+  require("blink-cmp").setup({
+    keymap = {
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-j>"] = { "snippet_forward", "fallback" },
+      ["<C-k>"] = { "snippet_backward", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-e>"] = { "hide" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
-  },
-})
+    windows = {
+      autocomplete = {
+        selection = "manual",
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+        update_delay_ms = 50,
+      },
+    },
+    trigger = {
+      signature_help = { enabled = true },
+    },
+    accept = { auto_brackets = { enabled = true } },
+  })
+  return true
+end)
+lz:events({ "InsertEnter" })
